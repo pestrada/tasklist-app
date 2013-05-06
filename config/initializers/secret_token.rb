baf4f9cf159cd87eb2e6c375986bfd9e5dee524c
pestrada <paul_e_mtz@hotmail.com>
2013-05-05 22:08:11 -0500

    creates application structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TasklistApp::Application.config.secret_token = '48dfe6b8776ea57c2dde731f5d704c4c0f2f090a6204fd66d66d89caaa6d3fa52d7e5f2612f093969c6d2f243a6aef9a14e0bc9b4e40bbb236f7110d629a4bcc'
