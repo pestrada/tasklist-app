dabe0f68cf48bebdc2ad1d07a1949da8752b811e
pestrada <paul_e_mtz@hotmail.com>
2013-05-18 16:26:59 -0500

    validetes status field

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :status
   
   validates :description, presence: true
+  validates :status, presence: true
 end
