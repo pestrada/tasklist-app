42632d9ca3810ff45a4582bd78b9f4b2bcbf40b0
pestrada <paul_e_mtz@hotmail.com>
2013-05-22 20:24:50 -0500

    #22 show pending tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,23 +5,22 @@ class Task < ActiveRecord::Base
   validates :status, presence: true
   
   def change_status(new_status)
-    case new_status
-    when 'finished'
-      self.status = new_status
-    when 'cancelled'
-      self.status = new_status
+    unless cancelled?
+      if pending? || finished?
+        self.status = new_status
+      end
     end
   end
   
   def pending?
-    self.status == 'pending'
+    status == 'pending'
   end
   
   def finished?
-    self.status == 'finished'
+    status == 'finished'
   end
   
   def cancelled?
-    self.status == 'cancelled'
+    status == 'cancelled'
   end
 end
